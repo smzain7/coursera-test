81087703c354633aa4c6ac187459a1f7e7a9c73a
Zain <zainshahid117@gmail.com>
2020-08-16 16:06:44 +0500

    portfolio

--- sample1/js/script.js
+++ sample1/js/script.js
@@ -1,4 +1,6 @@
 $(function() {
+
+  // HOME PAGE
   function incContrast (jumbotronx) {
     // var id=jumbotronx.id;
     document.getElementById(""+jumbotronx.id+"").style.filter = "contrast(120%)";
@@ -14,4 +16,40 @@ $(function() {
   }
   window.$incContrast = incContrast;
   window.$decContrast = decContrast;
+
+
+
+  // TURKEY PAGE
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  // var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " active";
+}
+
+window.$plusSlides=plusSlides;
+//window.$currentSlide=currentSlide;
+
 });
